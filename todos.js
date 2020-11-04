93f1f088b5896489217e6d2f10b9e77594783f6d
Laura Bassani <bassanilaurarita@gmail.com>
2020-11-04 12:12:32 +0100

    Create table with a json data

--- todos.js
+++ todos.js
@@ -1,10 +1,23 @@
+const todosTable =document.getElementById("todosTable");
 
+function studyList() {
+  let data = fetch("todos.json")
+    .then(response => response.json())
+    .then(data => { 
+      console.log(data);
+        
+      let rows =[];
 
-fetch("todos.json")
-  .then(response => response.json())
-  .then(data => { console.log(data)
-    //   let tasksList = JSON.parse(data)
-  })
-  .catch(e => console.log(e))
+      for(let i = 0; i < data.results.length; i++){
+        let row = document.createElement("tr");
+        row.innerHTML = "<td>" + data["results"][i]["title"] + "</td><td>" + data["results"][i]["priority"] + "</td>";
+        rows.push(row);
+        todosTable.appendChild(rows[i]);
+      }
+    })
+
+}
+
+studyList();
   
   
